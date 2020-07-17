edffae8f80f9f8e3574932a5b31789c32255ae14
geovani-moc <geovani.moc@outlook.com>
2020-07-17 11:19:31 -0300

    correcoes no frame

--- Frame.cpp
+++ Frame.cpp
@@ -12,21 +12,37 @@ int Frame::calcula_tamanho_do_frame()
     tamanho_frame = 8 * (int)(3 + posicoes_das_variaveis.size() + posicoes_dos_parametros.size());
 }
 
-int Frame::get_tamanho_do_frame(){return tamanho_frame;}
+int Frame::get_tamanho_do_frame() { return tamanho_frame; }
 
-int Frame::get_posicao_frame_anterior(){return posicao_frame_anterior;}
+int Frame::get_posicao_frame_anterior() { return posicao_frame_anterior; }
 
-int Frame::get_posicao_endereco_retorno(){return posicao_endereco_de_retorno;}
+int Frame::get_posicao_endereco_retorno() { return posicao_endereco_de_retorno; }
 
-int Frame::get_posicao_valor_de_retorno(){return posicao_valor_de_retorno;}
+int Frame::get_posicao_valor_de_retorno() { return posicao_valor_de_retorno; }
 
-int Frame::get_posicao_variavel()
+int Frame::get_posicao(string &nome)
 {
-    
+    int tamanho = posicoes_das_variaveis.size();
+    for (int i = 0; i < tamanho; i++)
+    {
+        if(posicoes_das_variaveis[i].first == nome) return posicoes_das_variaveis[i].second;
+    }
+
+    tamanho = posicoes_dos_parametros.size();
+    for (int i = 0; i < tamanho; i++)
+    {
+        if(posicoes_dos_parametros[i].first == nome) return posicoes_dos_parametros[i].second;
+    }
+
+    cerr << "\nErro: varivavel ou parametro nao encontrada no frame." << endl;
+    exit(EXIT_FAILURE);
 }
 
-int Frame::get_posicao_parametro()
+vector<pair<string, int>> identificar_variaveis(Arvore_parse &arvore)
 {
-
+    //No_arv_parse raiz = arvore.raiz;
 }
 
+vector<pair<string, int>> identificar_parametros(Arvore_parse &arvore)
+{
+}
--- Frame.hpp
+++ Frame.hpp
@@ -2,7 +2,7 @@
 #define FRAME_HPP
 
 #include <vector>
-#include <map>
+#include <utility>
 #include <string>
 
 #include "Arvore.hpp"
@@ -12,15 +12,15 @@ class Frame
 private://posicoes definidas em relacao ao stack pointer
     int posicao_frame_anterior;
     int posicao_endereco_de_retorno;
-    vector<map<string, int>> posicoes_das_variaveis;
-    vector<map<string, int>> posicoes_dos_parametros;
+    vector<pair<string, int>> posicoes_das_variaveis;
+    vector<pair<string, int>> posicoes_dos_parametros;
     int posicao_valor_de_retorno;
 
     int tamanho_frame;
 
     int calcula_tamanho_do_frame();
-    vector<map<string, int>> identificar_variaveis(Arvore_parse &arvore);
-    vector<map<string, int>> identificar_parametros(Arvore_parse &arvore);
+    vector<pair<string, int>> identificar_variaveis(Arvore_parse &arvore);
+    vector<pair<string, int>> identificar_parametros(Arvore_parse &arvore);
 
 public:
     Frame(Arvore_parse &arvore);
@@ -28,9 +28,8 @@ public:
     int get_tamanho_do_frame();
     int get_posicao_frame_anterior();
     int get_posicao_endereco_retorno();
-    int get_posicao_variavel();
-    int get_posicao_parametro();
     int get_posicao_valor_de_retorno();
+    int get_posicao(string &nome);
 };
 
 #endif
