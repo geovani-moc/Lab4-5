2b2c2b9853c3e98b32f9e036108b3de36f9c5091
geovani-moc <geovani.moc@outlook.com>
2020-07-21 16:02:56 -0300

    modificaçoes (contem erros)

--- RepresentacaoIntermediaria.cpp
+++ RepresentacaoIntermediaria.cpp
@@ -7,18 +7,35 @@ No_arvore_RI::No_arvore_RI()
 Arvore_RI::Arvore_RI(Arvore_parse &arvore_parse)
 {
     frame = Frame(arvore_parse);
-    raiz = NULL; //olhar se e necessario adicionar um seq aqui
+    raiz = NULL;
     contador_names = 0;
-    gerar_representacao(arvore_parse.raiz);
+    
+    gerar_representacao(arvore_parse);// tem que começar com um seq
+}
+
+void Arvore_RI::gerar_representacao(Arvore_parse &arvore_parse)
+{
+    raiz = new No_arvore_RI;
+
+    raiz->representacao.first = SEQ;
+    raiz->representacao.second = "";//colocar "seq"
+
+    //adicionar um SEQ para W e return E
+
+    raiz->derivacao.push_back(gerar_representacao(arvore_parse.raiz->filhos[7]));// tem que começar com um seq
+    raiz->derivacao.push_back(gerar_representacao(arvore_parse.raiz->filhos[9]));
 }
 
 No_arvore_RI* Arvore_RI::gerar_representacao(No_arv_parse *no_arvore_parse)
 {
+    No_arvore_RI * nodo;
     int tamanho = (int)no_arvore_parse->filhos.size();
+    string nome = no_arvore_parse->tok.nome;
 
     for (size_t i = 0; i < tamanho; ++i)
     {
-        if(no_arvore_parse->filhos[i]->regra != -1) return gerar_representacao(no_arvore_parse);
+        if(no_arvore_parse->filhos[i]->regra != -1) nodo = gerar_representacao(no_arvore_parse);
     }
     
+    return nodo;
 }
--- RepresentacaoIntermediaria.hpp
+++ RepresentacaoIntermediaria.hpp
@@ -6,13 +6,16 @@
 #include "Arvore.hpp"
 #include "Frame.hpp"
 
+#define SEQ (1)
+
 using namespace std;
 
 class No_arvore_RI
 {
 private:
     pair<int, string> representacao;
-    vector<No_arvore_RI*> derivacao;
+    vector<No_arvore_RI *> derivacao;
+
 public:
     No_arvore_RI();
 };
@@ -20,13 +23,15 @@ public:
 class Arvore_RI
 {
 private:
-    No_arvore_RI * raiz;
+    No_arvore_RI *raiz;
     Frame frame;
     int contador_names;
+
+    void gerar_representacao(Arvore_parse &arvore_parser);
+    No_arvore_RI *gerar_representacao(No_arv_parse *no_arvore_parse);
+
 public:
     Arvore_RI(Arvore_parse &arvore_parse);
-    No_arvore_RI* gerar_representacao(No_arv_parse *no_arvore_parse);
 };
-
 
 #endif
